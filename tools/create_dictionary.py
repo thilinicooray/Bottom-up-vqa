06aaec64f59aaca489803430041a02daa0e610c0
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-01-20 15:42:39 +0800

    question grouped by image added

--- tools/create_dictionary.py
+++ tools/create_dictionary.py
@@ -4,7 +4,7 @@ import sys
 import json
 import numpy as np
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from dataset import Dictionary
+from dataset_grouped import Dictionary
 
 
 def create_dictionary(dataroot):
